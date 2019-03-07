6f6d0e2b5f29ba6c7a03e8aff86f1c2533a974ee
Connor Bottum <cbb692@yahoo.com>
2019-03-07 15:59:14 -0800

    Added card.py

---
+++ card.py
@@ -0,0 +1,44 @@
+class Card:
+  def __init__(self, value, suit):
+    self.value = value
+    self.suit = suit
+    self.isHidden = False
+  
+  def faceDown(self):
+    self.isHidden = True
+  
+  def faceUp(self):
+    self.isHidden = False
+
+  def printCard(self):
+    card = self.toString()
+    print(card)
+
+  def toString(self):
+    val = self.value
+    if val == 1:
+      val = "A"
+    elif val == 11:
+      val = "J"
+    elif val == 12:
+      val = "Q"
+    elif val == 13:
+      val = "K"
+    else:
+      val = (str)(val)
+    return val + self.suit
+
+  '''
+  * @param c: The card we are comparing to 'self'
+  * @return: 1 if self > c, -1 if c > self, 0 if self = c
+  * Take 2 cards and evaluate which is a higher card
+  '''
+  def compareTo(self, c):
+    if c.type != Card:
+      raise ValueError("The passed object is not a card")
+    elif c.getValue > self.getValue():
+      return -1
+    elif c.getValue < self.getValue():
+      return 1
+    else: 
+      return 0
