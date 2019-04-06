8430f106926aa1335e7204f4ffa735640e189e45
c4llmeco4ch <cbb692@gmail.com>
2019-04-05 23:29:09 -0700

    Moved deal from deck tests to new deck document

--- bjUnitTests.py
+++ bjUnitTests.py
@@ -55,30 +55,6 @@ class TestAreBusted(unittest.TestCase):
         hand.addCard(Card(12, "H"))
         self.assertFalse(hand.areBusted())
 
-class TestDealCardsFromDeck(unittest.TestCase):
-
-    def testNoShuffleDeal(self):
-        d = Deck()
-        answer = d.dealCard()
-        self.assertEqual(answer.value, 1)
-        self.assertEqual(answer.suit, "H")
-        self.assertFalse(d.isEmpty)
-        self.assertEqual(d.size, 51)
-
-    def testLastCard(self):
-        d = Deck()
-        for i in range(len(d.deck) - 1):
-            d.dealCard()
-        self.assertFalse(d.isEmpty)
-        d.dealCard()
-        self.assertTrue(d.isEmpty)
-        self.assertIsNone(d.dealCard())
-
-    def testShuffleMaintainsSize(self):
-        d = Deck()
-        d.shuffle()
-        self.assertEqual(d.size, 52)
-
 class TestStanding(unittest.TestCase):
 
     def testDualAces(self):
---
+++ deckUnitTests.py
@@ -0,0 +1,27 @@
+from card import Card
+import random
+import unittest
+
+class TestDealCardsFromDeck(unittest.TestCase):
+
+    def testNoShuffleDeal(self):
+        d = Deck()
+        answer = d.dealCard()
+        self.assertEqual(answer.value, 1)
+        self.assertEqual(answer.suit, "H")
+        self.assertFalse(d.isEmpty)
+        self.assertEqual(d.size, 51)
+
+    def testLastCard(self):
+        d = Deck()
+        for i in range(len(d.deck) - 1):
+            d.dealCard()
+        self.assertFalse(d.isEmpty)
+        d.dealCard()
+        self.assertTrue(d.isEmpty)
+        self.assertIsNone(d.dealCard())
+
+    def testShuffleMaintainsSize(self):
+        d = Deck()
+        d.shuffle()
+        self.assertEqual(d.size, 52)
